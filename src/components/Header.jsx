6b373f5c1fe41716ea5f14ad0d5b036f7de4dfd8
bcline01 <brookecline.bc@gmail.com>
2024-09-17 11:36:08 -0400

    styled portfolio content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,11 @@ function Header() {
             
             <a href="https://github.com/bcline01" target="_blank" rel="noopener noreferrer">
                
-                <FaGithub style={{color: 'white', position: 'fixed', right:'20px', fontSize: '30px' }}/>
+                <FaGithub style={{color: 'white', position: 'fixed', right:'20px', fontSize: '40px' }}/>
 
             </a>
             <a href="https://www.linkedin.com/in/brookecline23/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin style={{color: 'white', position: 'fixed', right:'65px', fontSize: '30px' }}/>
+            <FaLinkedin style={{color: 'white', position: 'fixed', right:'65px', fontSize: '40px' }}/>
             </a>
             <NavTabs/>
 
