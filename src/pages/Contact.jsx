666ffe5b7cf5b51b63401a92444409f4a4f1ce6e
bcline01 <brookecline.bc@gmail.com>
2024-09-17 10:51:40 -0400

    styled my contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 import Form from '../components/Form.jsx';
-
+import '../styles/Contact.css'
 
 export default function Contact() {
     return (
       <div>
-        <h1 style={{color: 'lightgray'}}>Contact Me</h1>
+        <h1>Contact Me!</h1>
         <Form />
       </div>
     );
